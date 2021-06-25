acb33cbf38bb5887fc9d2292e96c6f0d802f378f
Lukylix <lucas.garcia.bobi@gmail.com>
2021-06-25 16:20:59 +0200

    Typo fix

--- configs/mongoose.js
+++ configs/mongoose.js
@@ -1,10 +1,8 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-
 //Initialise mongoose connection and handle initial error
-mongoose.connect(DB.uri, DB.options).catch((err) => console.error(err));
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }).catch((err) => console.error(err));
 //Handle errors after initial connection
 mongoose.connection.on("error", (err) => console.error(err));
 //Listen for the first emit of the "open" event (equivalent to the connected event)
--- models/User.js
+++ models/User.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
-const {Schema, model} = mongoose;
+const { Schema, model } = mongoose;
 
 exports.schema = new Schema({
-  username: {type: String, required = true, unique = true}
-})
+  username: { type: String, required: true, unique: true },
+});
 
 exports.model = model("User", exports.schema);
