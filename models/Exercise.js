5fa40ac10fdd7ea0b5e64f73f3ad40630f562576
Lukylix <lucas.garcia.bobi@gmail.com>
2021-06-25 15:47:29 +0200

    Models

---
+++ models/Exercise.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const {Schema, model} = mongoose;
+
+exports.schema = new Schema({
+  user_id: { type: Schema.Types.ObjectId, ref: 'User', required = true},
+  description: {type: String, required : true},
+  duration: {type: Number, required: true},
+  date:{ type: Date, default: Date.now()}
+})
+
+exports.model = model("Exercise", exports.schema);
